83309721f6dd71b736de54b1c85c5776ef82df55
sarath <sarath@cabin-holiday.com>
2019-02-13 07:15:52 +0100

    Doctrine create and update data

--- src/AppBundle/Controller/CrudController.php
+++ src/AppBundle/Controller/CrudController.php
@@ -12,6 +12,7 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CrudController extends Controller
@@ -55,6 +56,27 @@ class CrudController extends Controller
         }
 
         return $this->render('AppBundle::listIndividual.html.php', ['product' => $product]);
+    }
+
+    /**
+     * @Route("/listMultipleId", name="list_one")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function listMultipleIdAction()
+    {
+        $products = $this->getDoctrine()
+            ->getRepository(Product::class)
+            ->findBy([
+                'id' => [1,3]
+            ]);
+
+        if (!$products) {
+            throw $this->createNotFoundException(
+                'No product found'
+            );
+        }
+
+        return $this->render('AppBundle::listMltipleId.html.php', ['products' => $products]);
     }
 
     /**
@@ -102,4 +124,49 @@ class CrudController extends Controller
         ]);
     }
 
+    /**
+     * @Route("/create")
+     */
+    public function createAction()
+    {
+        // you can fetch the EntityManager via $this->getDoctrine()
+        // or you can add an argument to your action: createAction(EntityManagerInterface $entityManager)
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $product = new Product();
+        $product->setName('Nokia');
+        $product->setPrice(19000);
+        $product->setDescription('Mobile');
+
+        // tells Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($product);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        //return new Response('Saved new product with id '.$product->getId()); //OR
+        return $this->redirectToRoute('list');
+    }
+
+    /**
+     * @Route("/update/{productID}")
+     * @param int $productID
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function updateAction($productID)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $product       = $entityManager->getRepository(Product::class)->find($productID);
+
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$productID
+            );
+        }
+
+        $product->setName('Redmi');
+        $entityManager->flush();
+
+        return $this->redirectToRoute('list');
+    }
 }
